e44e51c923a85468785bb0e47d98ec104618d40c
yadavbishnu <bishnukyadav07@gmail.com>
2024-07-13 14:21:38 -0400

    update on index.html
    Update the assignment 4 part 1. Here I have added comments and div with customname. (second commit)

--- Assignment 4/js/main.js
+++ Assignment 4/js/main.js
@@ -1,4 +1,16 @@
+function randomValueFromArray(array){
+    const random = Math.floor(Math.random()*array.length);
+    return array[random];
+  }
+
 function result(){
     let newStory = storyText;
-    
+
+    const xItem = randomValueFromArray(insertX);
+    const yItem = randomValueFromArray(insertY);
+    const zItem = randomValueFromArray(insertZ);
+
+    newStory = newStory.replaceAll(':insertx:',xItem);
+    newStory = newStory.replaceAll(':inserty:',yItem);
+    newStory = newStory.replaceAll(':insertz:',zItem);
 }
